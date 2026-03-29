4ab73b88f2a79c4de50535d5b84386a15079fd56
Anteneh Wondwosen <antenehwondwosen@gmail.com>
2026-03-29 18:53:20 +0200

    addedn workout scheduling logic

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-
+app.use('/api/workouts', workoutRoutes);
 
 // Root route
 app.get('/', (req, res) => {
--- backend/models/Workout.js
+++ backend/models/Workout.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const WorkoutSchema = new mongoose.Schema({
     name: {
