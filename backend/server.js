013d793d4fffeaf5062afe4edbf00ee3338c0bd5
Emnet19 <emnbef456@gmail.com>
2026-04-06 16:13:51 +0300

    server update

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import { NODE_ENV } from './config/env.js';
 
 connectDB();
 
-const serverPort = PORT || 5000;
+const serverPort = process.env.PORT || 5000;
 
 app.listen(serverPort, () => {
     console.log(`Server running in ${NODE_ENV} mode on port ${serverPort}`);
