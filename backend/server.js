857ac0a10afb825c7aa398a84f8f8c4e03f87a07
Emnet19 <emnbef456@gmail.com>
2026-04-06 16:10:22 +0300

    server update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import './config/env.js'; 
 import app from './app.js';
 import connectDB from './config/db.js';
-import { PORT, NODE_ENV } from './config/env.js';
+import { NODE_ENV } from './config/env.js';
 
 connectDB();
 
